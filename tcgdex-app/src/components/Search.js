731086bb13d66b5ccebd138978e16886713566cc
Reese Ganaway <reeseganaway@gmail.com>
2022-01-24 14:29:17 -0500

    Added nav bar to search page

--- tcgdex-app/src/components/Search.js
+++ tcgdex-app/src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Search() {
   const [pokename, setPokename] = useState("");
@@ -25,6 +26,23 @@ export default function Search() {
   };
   return (
     <Fragment>
+      <div className="NavBars">
+        <Link className="Links" to="/home">
+          Home
+        </Link>
+        <Link className="Links" to="/userProfile">
+          UserProfile
+        </Link>
+        <Link className="Links" to="/login">
+          Login
+        </Link>
+        <Link className="Links" to="/collection">
+          Collection
+        </Link>
+        <Link className="Links" to="/search">
+          Search
+        </Link>
+      </div>
       <form>
         <input
           placeholder="Search Pokemon Name"
