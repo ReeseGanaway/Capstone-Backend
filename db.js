6905c5502fe6fd7a4fc10e57f7a56c23dc76bb4c
Reese Ganaway <reeseganaway@gmail.com>
2022-02-10 01:16:06 -0500

    Changed port in pool

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
   host: `${process.env.DB_HOST}`,
-  port: process.env.DB_PORT || 5000,
+  port: process.env.DB_PORT,
   database: `${process.env.DB_DATABASE}`,
   ssl: {
     rejectUnauthorized: false,
