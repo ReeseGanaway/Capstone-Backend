d554b9a6d671d5e8fccc18d0302a724e5d64691b
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2022-02-25 20:48:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const compression = require("compression");
 const db = require("./db");
 
+res.header('Access-Control-Allow-Origin', '*');
+  next();
+
 //middleware
 app.use(cors());
 app.use(express.json());
